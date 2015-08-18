6cdd2b7d3aa84a2f2867dad37a5d3c0f2b144d55
jlsears <jen.lauren.sears@gmail.com>
2015-08-17 21:21:59 -0500

    initial commit

---
+++ ShapesP2/IShape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesP2
+{
+    public interface IShape //Interface - list of method somethingsomething
+    {
+        float Area();
+        float Perimeter();
+    }
+}
---
+++ ShapesP2/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ShapesP2
+{
+    public abstract class Shape : IShape
+    {
+        // We need info about colors
+        // Need to peer into wpf framework to see how to even define color something can be drawn
+        Color fillColor;
+        Color borderColor;
+        
+        public Color FillColor { get;  }
+        public Color BorderColor { get; }
+
+        // Need to make sure these methods can be overwritten
+
+        public float Area() { throw new NotImplementedException(); }
+        public float Perimeter() { throw new NotImplementedException(); }
+    }
+}
